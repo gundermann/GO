b3c756205318247ff11a73bfc5239eb74ff15664
Niels Gundermann <gundermann.niels.ng@googlemail.com>
2014-09-11 17:17:29 +0200

    initinal commit

---
+++ de.nordakademie.wpk.tasklist.product/src/de/nordakademie/wpk/tasklist/product/handler/RefreshHandler.java
@@ -0,0 +1,12 @@
+ 
+package de.nordakademie.wpk.tasklist.product.handler;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class RefreshHandler {
+	@Execute
+	public void execute() {
+		
+	}
+		
+}
