92eb7ab5b5c00f5740040fc1608a4ebfb87aeb39
Niels Gundermann <gundermann.niels.ng@googlemail.com>
2014-09-15 15:35:46 +0200

    Refreshhandler implementiert

--- de.nordakademie.wpk.tasklist.product/src/de/nordakademie/wpk/tasklist/product/handler/RefreshHandler.java
+++ de.nordakademie.wpk.tasklist.product/src/de/nordakademie/wpk/tasklist/product/handler/RefreshHandler.java
@@ -1,12 +1,24 @@
  
 package de.nordakademie.wpk.tasklist.product.handler;
 
+import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 
+import de.nordakademie.wpk.tasklist.settings.ui.jobs.LoadAllJob;
+
 public class RefreshHandler {
+	
+	
+	
 	@Execute
 	public void execute() {
-		
+		//TODO INJECT
+		new LoadAllJob().schedule();
+	}
+	
+	@CanExecute
+	public boolean canExecute(){
+		return true;
 	}
 		
 }
