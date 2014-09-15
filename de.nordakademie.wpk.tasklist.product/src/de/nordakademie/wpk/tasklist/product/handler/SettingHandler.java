d8727353d20c2fdf7cdfd128a146831b9c4e9840
Niels Gundermann <gundermann.niels.ng@googlemail.com>
2014-09-15 09:41:13 +0200

    Einstellungsmenü

---
+++ de.nordakademie.wpk.tasklist.product/src/de/nordakademie/wpk/tasklist/product/handler/SettingHandler.java
@@ -0,0 +1,12 @@
+ 
+package de.nordakademie.wpk.tasklist.product.handler;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class SettingHandler {
+	@Execute
+	public void execute() {
+		
+	}
+		
+}
