1d5132905b62a01b2592a8f636a37cc5bab66198
CaraFeelester <carafeelester@gmail.com>
2014-09-15 16:08:18 +0200

    überflüssigen EditSettingsHandler entfernt -> richtiger Handler ist SettingsHandler im product

--- de.nordakademie.wpk.tasklist.product/src/de/nordakademie/wpk/tasklist/product/handler/SettingHandler.java
+++ de.nordakademie.wpk.tasklist.product/src/de/nordakademie/wpk/tasklist/product/handler/SettingHandler.java
@@ -1,13 +1,36 @@
  
 package de.nordakademie.wpk.tasklist.product.handler;
 
-import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.handlers.HandlerUtil;
 
-public class SettingHandler {
+import de.nordakademie.wpk.tasklist.settings.ui.EditSettingsDialog;
+
+public class SettingHandler extends AbstractHandler{
 	
-	@Execute
-	public void execute() {
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
 		
+		Shell shell = HandlerUtil.getActiveShell(event);
+		EditSettingsDialog dialog = new EditSettingsDialog(shell, shell.getStyle());
+		
+		int result = dialog.open();
+		
+		if(result == IStatus.OK){
+			
+			// Einstellungen werden gespeichert
+			MessageDialog.openInformation(shell, "Nicht implementiert", "Speichern der Einstellungen noch nicht implementiert!");
+		}
+		else{
+			MessageDialog.openError(shell, "Abgebrochen", "Einstellungen wurden nicht gespeichert");
+		}
+		
+		return null;
 	}
 		
 }
