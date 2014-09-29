728535d1449c32198dfb54a58a41ea1879b26753
Niels Gundermann <gundermann.niels.ng@googlemail.com>
2014-09-29 15:46:32 +0200

    Kann sich exportieren lassen ZWISCHENSTAND !!!

--- de.nordakademie.wpk.tasklist.core.server/src/main/java/de/nordakademie/wpk/tasklist/core/server/service/google/GoogleProvider.java
+++ de.nordakademie.wpk.tasklist.core.server/src/main/java/de/nordakademie/wpk/tasklist/core/server/service/google/GoogleProvider.java
@@ -32,37 +32,22 @@ public class GoogleProvider implements ProviderService {
 		return tasksService;
 	}
 
-	/* (non-Javadoc)
-	 * @see de.nordakademie.wpk.tasklist.core.server.service.google.ProviderService#updateTaskList(de.nordakademie.wpk.tasklist.core.api.TaskList, de.nordakademie.wpk.tasklist.core.api.ProviderSetting)
-	 */
 	public void updateTaskList(TaskList tasklist) {
 		googleConverter.updateTaskList(getTaskService(setting), tasklist);
 	}
 
-	/* (non-Javadoc)
-	 * @see de.nordakademie.wpk.tasklist.core.server.service.google.ProviderService#addTaskList(de.nordakademie.wpk.tasklist.core.api.TaskList, de.nordakademie.wpk.tasklist.core.api.ProviderSetting)
-	 */
 	public void addTaskList(TaskList tasklist) {
 		googleConverter.addTasklist(getTaskService(setting), tasklist);
 	}
 
-	/* (non-Javadoc)
-	 * @see de.nordakademie.wpk.tasklist.core.server.service.google.ProviderService#updateTask(de.nordakademie.wpk.tasklist.core.api.Task, java.lang.String, de.nordakademie.wpk.tasklist.core.api.ProviderSetting)
-	 */
 	public void updateTask(Task task, String tasklistId) {
 		googleConverter.updateTask(getTaskService(setting), task, tasklistId);
 	}
 
-	/* (non-Javadoc)
-	 * @see de.nordakademie.wpk.tasklist.core.server.service.google.ProviderService#addTask(de.nordakademie.wpk.tasklist.core.api.Task, java.lang.String, de.nordakademie.wpk.tasklist.core.api.ProviderSetting)
-	 */
 	public void addTask(Task task, String tasklistId) {
 		googleConverter.addTask(getTaskService(setting), task, tasklistId);
 	}
 
-	/* (non-Javadoc)
-	 * @see de.nordakademie.wpk.tasklist.core.server.service.google.ProviderService#loadTask(java.lang.String, java.lang.String, de.nordakademie.wpk.tasklist.core.api.ProviderSetting)
-	 */
 	public Task loadTask(String taskId, String tasklistId) {
 		return googleConverter.convertTask(getTaskService(setting), taskId, tasklistId);
 	}
