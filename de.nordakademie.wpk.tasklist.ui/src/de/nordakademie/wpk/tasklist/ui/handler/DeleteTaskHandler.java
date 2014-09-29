24e2db9dbb7145c9476d6fec6a781fa1dbb486fb
Niels Gundermann <gundermann.niels.ng@googlemail.com>
2014-09-29 12:56:56 +0200

    Abfragen beim löschen der Tasks und Tasklisten

--- de.nordakademie.wpk.tasklist.ui/src/de/nordakademie/wpk/tasklist/ui/handler/DeleteTaskHandler.java
+++ de.nordakademie.wpk.tasklist.ui/src/de/nordakademie/wpk/tasklist/ui/handler/DeleteTaskHandler.java
@@ -1,13 +1,15 @@
- 
 package de.nordakademie.wpk.tasklist.ui.handler;
 
 import javax.inject.Inject;
 
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ITreeSelection;
+import org.eclipse.swt.widgets.Shell;
 
 import de.nordakademie.wpk.tasklist.core.api.Task;
 import de.nordakademie.wpk.tasklist.core.api.TaskList;
@@ -15,26 +17,34 @@ import de.nordakademie.wpk.tasklist.core.api.TaskService;
 import de.nordakademie.wpk.tasklist.ui.jobs.DeleteTaskJob;
 
 public class DeleteTaskHandler {
+
+	private String message = "Wollen Sie die Task wirklich löschen?";
+
 	@Inject
 	private ESelectionService selectionService;
-	
+
 	@Inject
 	private TaskService taskService;
-	
-	@Inject 
+
+	@Inject
 	private IEventBroker eventBorker;
-	
+
 	@Execute
-	public void execute() {
+	public void execute(Shell shell) throws ExecutionException {
 		if (canExecute()) {
-			ITreeSelection selection = (ITreeSelection) selectionService.getSelection();
-			Task task = (Task) selection.getFirstElement();
-			TaskList tasklist = (TaskList) selection.getPaths()[0].getSegment(1);
-			new DeleteTaskJob(tasklist.getId(), task.getId(), taskService, eventBorker).schedule();
+			boolean confirmed = MessageDialog.openQuestion(shell, "Wirklich?", message);
+			if (confirmed) {
+				ITreeSelection selection = (ITreeSelection) selectionService
+						.getSelection();
+				Task task = (Task) selection.getFirstElement();
+				TaskList tasklist = (TaskList) selection.getPaths()[0]
+						.getSegment(1);
+				new DeleteTaskJob(tasklist.getId(), task.getId(), taskService,
+						eventBorker).schedule();
+			}
 		}
 	}
-	
-	
+
 	@CanExecute
 	public boolean canExecute() {
 		if (selectionService.getSelection() instanceof ITreeSelection) {
@@ -43,5 +53,5 @@ public class DeleteTaskHandler {
 		}
 		return false;
 	}
-		
+
 }
--- de.nordakademie.wpk.tasklist.ui/src/de/nordakademie/wpk/tasklist/ui/handler/DeleteTasklistHandler.java
+++ de.nordakademie.wpk.tasklist.ui/src/de/nordakademie/wpk/tasklist/ui/handler/DeleteTasklistHandler.java
@@ -1,13 +1,17 @@
  
 package de.nordakademie.wpk.tasklist.ui.handler;
 
+import java.util.concurrent.ExecutionException;
+
 import javax.inject.Inject;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ITreeSelection;
+import org.eclipse.swt.widgets.Shell;
 
 import de.nordakademie.wpk.tasklist.core.api.Task;
 import de.nordakademie.wpk.tasklist.core.api.TaskList;
@@ -25,13 +29,18 @@ public class DeleteTasklistHandler {
 	
 	@Inject
 	private TaskService taskService;
+
+	private String message = "Wollen Sie die Taskliste wirklich löschen? Alle Tasks in der Taskliste werden mit gelöscht!";
 	
 	@Execute
-	public void execute() {
+	public void execute(Shell shell ) throws ExecutionException{
 		if (canExecute()) {
+			boolean confirmed = MessageDialog.openQuestion(shell, "Wirklich?", message );
+			if (confirmed) {
 			ITreeSelection selection = (ITreeSelection) selectionService.getSelection();
 			TaskList tasklist = (TaskList) selection.getFirstElement();
 			new DeleteTasklistJob(tasklist.getId(), taskService, eventBroker).schedule();
+			}
 		}
 	}
 	
