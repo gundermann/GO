a23f23a4edafbc3a69309e7519e99b8e44f37706
Niels Gundermann <gundermann.niels.ng@googlemail.com>
2014-10-02 17:12:33 +0200

    Refactoring

--- de.nordakademie.wpk.tasklist.settings.ui/src/de/nordakademie/wpk/tasklist/settings/ui/EditSettingsDialog.java
+++ de.nordakademie.wpk.tasklist.settings.ui/src/de/nordakademie/wpk/tasklist/settings/ui/EditSettingsDialog.java
@@ -15,7 +15,6 @@ import org.eclipse.swt.widgets.Text;
 
 import de.nordakademie.wpk.tasklist.core.api.Provider;
 import de.nordakademie.wpk.tasklist.core.api.ProviderSetting;
-import de.nordakademie.wpk.tasklist.core.api.ProviderSetting;
 import de.nordakademie.wpk.tasklist.core.client.NoSettingFoundException;
 import de.nordakademie.wpk.tasklist.core.client.ProviderSettingContainer;
 import de.nordakademie.wpk.tasklist.core.client.SettingSaver;
